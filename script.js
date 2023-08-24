d513d9e08526f6b33d2c5bfe44113da61ffe778a
naveen-kk4 <133044450+naveen-kk4@users.noreply.github.com>
2023-08-25 04:53:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If required
+
+	 new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("HELLO , world!");
+    },1000)
+}).then((value)=>{
+    document.getElementById("output").innerHTML = value;
+});
+
